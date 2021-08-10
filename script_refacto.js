1311169212469e0cf35b9528dce7ac87d62f50f2
DinaTaklit <ez_taklit@esi.dz>
2021-08-10 06:45:15 +0100

    feat: script_refacto: implement graphDataset Function that create the graph dataset

--- script_refacto.js
+++ script_refacto.js
@@ -1,13 +1,57 @@
 import data from './data.js'
 
+// DataSet strucutre 
+const datasetStructure = {
+    label: "", 
+    data: [],
+    backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(255, 206, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+    ],
+    borderColor: [
+      'rgba(255, 99, 132, 1)',
+      'rgba(54, 162, 235, 1)',
+      'rgba(255, 206, 86, 1)',
+      'rgba(75, 192, 192, 1)'
+    ], 
+}
 
 // Function thart create the graph
 const graph = () =>{
     const newData = dataWithAvgTotal() // get new data with average total
     const labels = graphLabels() // get graph labels
-    console.log(newData)
-    console.log(labels)
+
+    let keys = Object.keys(newData[0]) // get the keys of the object
+    keys = keys.filter(key => key !== 'period') //  exclude the period key
+
+    const dataset = graphDataset(keys, newData) // get the dataset
+    console.log(dataset)
 }
+
+// Function that create the graph Dataset
+const graphDataset = (keys, data) => {
+    // Loop through all the keys
+    const dataset = keys.map(key => {
+        // For each key add all it is value from the whole data
+        const newItem = data.reduce((newItem, item) => {
+            return {
+                ...newItem,
+                data: [...newItem.data, item[key]] // append the item value
+            }
+        }, datasetStructure)
+        
+        // reuturn the newItem
+        return {
+            ...newItem,
+            label: key,
+        }
+
+    })
+    return dataset
+}
+
 
 // Function that geenrate the average total of each record
 const dataWithAvgTotal = () => {
