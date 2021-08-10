7922a927e99ad01cbd19c1b1f5562d40e595be2e
DinaTaklit <ez_taklit@esi.dz>
2021-08-10 05:54:10 +0100

    feat: script_refacto: implement dataWithAvgTotal function that return new array of data objects with average total

--- script_refacto.js
+++ script_refacto.js
@@ -1,2 +1,21 @@
 import data from './data.js'
-console.log(data)
+
+// Function that geenrate the average total of each record
+const dataWithAvgTotal = () => {
+    // array of the average values of each data record
+    const newdata = data.map(item => {
+        const keys = Object.keys(item) // get the keys of the object
+        // claculate the total of the object items using reduce function
+        const total = keys.reduce((total, key) => {
+             if (key !== 'period') { // exclude the period key
+                 return total + item[key]
+             } 
+             return total
+         }, 0)   
+         return {
+            ...item,
+            "avgTotal":total / (keys.length - 1) //  average value
+         }
+     })
+    return newdata
+}
